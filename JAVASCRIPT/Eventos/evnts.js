453e2101d2e44cdef2bace893754836f115859c7
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-03-29 00:03:50 -0300

    Adicionando eventos no JS

---
+++ JAVASCRIPT/Eventos/evnts.js
@@ -0,0 +1,8 @@
+// Evento	                Descrição
+
+// onchange	                Um elemento HTML foi alterado.
+// onclick	                O usuário clica em um elemento HTML.
+// ao passar o mouse	    O usuário move o cursor do mouse sobre um elemento HTML.
+// onmouseout	            O usuário move o cursor do mouse para longe de um elemento HTML.
+// onkeydown	            O usuário pressiona uma tecla do teclado.
+// onload	                O navegador terminou de carregar a página.
