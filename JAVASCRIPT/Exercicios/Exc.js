2fafb9292ddd76dfb9cdf7a525b162230922b4c6
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-04-07 22:44:36 -0300

    Realizando/treinando alguns exercícios em JavaScript

--- JAVASCRIPT/Exercicios/Exc.js
+++ JAVASCRIPT/Exercicios/Exc.js
@@ -19,6 +19,14 @@
 
 
 
+
+
+
+
+
+
+
+
 // Crie um programa em Python que: Crie uma constante chamada PI com o valor 3.14159. Crie uma variável chamada raio e atribua o valor 5. Calcule a área do círculo (fórmula: PI * raio * raio) e guarde em uma variável chamada area. Imprima na tela o resultado. Crie um outro programa em Python que: Pergunte ao usuário seu nome e sua idade. Imprima a mensagem: Olá, <nome>! Você tem <idade> anos.
 
 // const pi = 3.14159
@@ -48,14 +56,98 @@
 
 
 
+
+
+
+
+
+
+
+
+
 // Projeto Prático Crie um programa em Python que interaja com o usuário para coletar dados pessoais (nome, idade, cidade), calcular o IMC (com peso e altura informados), e informar se a pessoa está com peso ideal ou não. O programa deve usar os conceitos de variáveis, tipos e entrada/saída. Requisitos: Pedir ao usuário o nome, idade, cidade, peso e altura. Calcular o IMC = peso / (altura * altura) Mostrar uma mensagem de boas-vindas com o nome e cidade. Mostrar a idade do usuário. Mostrar o valor do IMC com 2 casas decimais. Tratar entradas para garantir que peso e altura sejam números válidos.
 
-const nome = prompt("Digite seu nome:");
-const idade = prompt("Digite sua idade:");
-const cidade = prompt("Digite sua cidade:"); 
-const peso = parseFloat(prompt("Digite seu peso:"));
-const altura = parseFloat(prompt("Digite sua altuta:"));
+// const nome = prompt("Digite seu nome:");
+// const idade = prompt("Digite sua idade:");
+// const cidade = prompt("Digite sua cidade:"); 
+// const peso = parseFloat(prompt("Digite seu peso:"));
+// const altura = parseFloat(prompt("Digite sua altuta:"));
 
-const IMC = peso / (altura * altura);
+// const IMC = peso / (altura * altura);
 
-console.log("Seja bem vindo," + nome + "residente da" + cidade + "sua idade é" + idade + "seu imc é" + IMC.toFixed(2));
+// console.log(`Seja bem vindo, ${nome}  residente da cidade de ${cidade}.  Sua idade é ${idade}  seu imc é  ${IMC.toFixed(2)}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Crie um programa em Python que: Peça ao usuário duas notas para dois alunos (total de 4 notas). Calcule a média de cada aluno. Exiba para cada aluno a mensagem correspondente: "Aprovado" se a média for maior ou igual a 7; "Recuperação" se a média for maior ou igual a 5 e menor que 7; "Reprovado" se a média for menor que 5.
+
+// let notaa = parseFloat(prompt("Digite uma nota para o aluno A:"));
+// let notab = parseFloat(prompt("Digite uma nota para o aluno B:"));
+
+// let media = (notaa + notab) / 2;
+
+// function resultado(media) {
+//     if (media >= 7){
+//         return "Aprovado!";
+//     }else if (media >= 5){
+//         return "Recuperacao!"
+//     }else{
+//         return "Reprovado!"
+//     }
+// }
+
+// console.log("Media:", media.toFixed(2));
+// console.log("Resultado:", resultado(media));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Crie um programa que solicite ao usuário: Idade da pessoa Se possui carteira de motorista (True ou False) O programa deve verificar se a pessoa pode dirigir. Para isso, a pessoa precisa ter 18 anos ou mais e ter carteira. Exiba na tela: "Pode dirigir" se a pessoa atender aos critérios "Não pode dirigir" caso contrário Dica: use os operadores >= e and.
+
+let idade = parseInt(prompt("Digite sua idade:"));
+let cnh = prompt("Possui carteira ? (S/N):");
+
+if (idade >= 18 && cnh.toUpperCase() === "S"){
+    console.log("Pode Dirigir");
+}else{
+    console.log("Nao pode Dirigir");
+}
