8f60a12c0847bc0f02467d1e1ae7e37098b6f010
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2026-04-21 01:58:49 -0300

    Introdução teorica de DOM

---
+++ JAVASCRIPT/DOM/script.js
@@ -0,0 +1,36 @@
+// O DOM HTML (Modelo de Objeto de Documento HTML) é um modelo de objeto para documentos HTML .
+
+
+
+//ACEESSANDO HTML 
+
+// O DOM HTML pode ser usado para acessar elementos HTML.
+// A forma mais comum de acessar um elemento HTML é usando o atributo `id` iddo elemento:
+
+
+
+
+// <html>
+// <body>
+
+// <p id="demo"></p>
+
+// <script>
+// // Access a paragraph Element
+// const myPara = document.getElementById("demo");
+
+// // Change the content of the Element
+// myPara.innerHTML = "Hello World!";
+// </script>
+
+// </body>
+// </html>
+
+
+
+
+
+// No exemplo acima, o getElementById método usado id="demo"para encontrar o elemento.
+// id="demo"é uma propriedade HTML
+// getElementById()é um método DOM
+// innerHTMLé uma propriedade DOM
